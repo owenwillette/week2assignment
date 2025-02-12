9ff33a2223743e08026f2006bac4b966c974667e
owenwillette <owenwillette94@gmail.com>
2025-02-11 19:22:33 -0500

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,16 @@
+finished_program = False
+
+# Count to 10 Program
+
+i = 0
+
+while finished_program == False:
+
+    
+
+    print(f'Testing i = {i}')
+
+    if i == 10:
+        finished_program = True
+
+    i += 1
